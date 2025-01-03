9be3cb4464c69ce376586c200b1a2a27932effca
Om Charhate <145870592+charhateom@users.noreply.github.com>
2025-01-03 04:29:15 +0000

    datypes

---
+++ 01_Basics/02_datatype.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version 
+
+//alert(3+3) // we are using node not browser
+
+let name = "Hitesh"
+let age = 18 
+let isLoggedIn = false 
+let state = null 
+// number => 2 to power 53 
+//bigint  
+//string => ""
+//boolean => true / false 
+// null => standalone value 
+// undefined => 
+// symbol 
+
+
+// object 
+
+console.log(typeof "Hitesh") //string
+console.log(typeof undefined); //undefined 
+
+
+console.log(typeof null); // object 
